25c6e04556e1f3fa3afe74e33aa79c29681b5921
shaynlink <guemmoudrayane03@gmail.com>
2022-10-11 01:06:11 +0200

    course: Update Data from MongoDB

--- src/index.js
+++ src/index.js
@@ -69,23 +69,48 @@ const client = new MongoClient(uri);
 //     }
 // }
 
+// COURSE: Update Data in MongoDB
+
+// async function run() {
+//     try {
+//         await client.connect();
+//         // database and collection code goes here
+//         const db = client.db('sample_guides');
+//         const coll = db.collection('comets');
+//         // insert code goes here
+//         const docs = [
+//             {name: "Halley's Comet", officialName: "1P/Halley", orbitalPeriod: 75, radius: 3.4175, mass: 2.2e14},
+//             {name: "Wild2", officialName: "81P/Wild", orbitalPeriod: 6.41, radius: 1.5534, mass: 2.3e13},
+//             {name: "Comet Hyakutake", officialName: "C/1996 B2", orbitalPeriod: 17000, radius: 0.77671, mass: 8.8e12}
+//         ];
+  
+//         const result = await coll.insertMany(docs);
+  
+//         // display the results of your operation
+//         console.log(result.insertedIds);
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+
 async function run() {
     try {
         await client.connect();
         // database and collection code goes here
         const db = client.db('sample_guides');
         const coll = db.collection('comets');
-        // insert code goes here
-        const docs = [
-            {name: "Halley's Comet", officialName: "1P/Halley", orbitalPeriod: 75, radius: 3.4175, mass: 2.2e14},
-            {name: "Wild2", officialName: "81P/Wild", orbitalPeriod: 6.41, radius: 1.5534, mass: 2.3e13},
-            {name: "Comet Hyakutake", officialName: "C/1996 B2", orbitalPeriod: 17000, radius: 0.77671, mass: 8.8e12}
-        ];
-  
-        const result = await coll.insertMany(docs);
-  
-        // display the results of your operation
-        console.log(result.insertedIds);
+        // update code goes here
+        const filter = { };
+        const updateDoc = {
+            $mul: {
+                radius: 1.60934
+            }
+        };
+
+        const result = await coll.updateMany(filter, updateDoc);
+        // amount deleted code goes here
+        console.log('Number of documents updated: %s', result.modifiedCount);
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
