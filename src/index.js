b7ee23e99ccbd3cbc92e7bf5c1919dee36ce4a08
shaynlink <guemmoudrayane03@gmail.com>
2022-10-11 00:43:54 +0200

    course: Read Data from MongoDB With Queries

--- src/index.js
+++ src/index.js
@@ -4,7 +4,25 @@ const { MongoClient } = require('mongodb');
 
 const uri = process.env.MONGO_URI;
 
-    const client = new MongoClient(uri);
+const client = new MongoClient(uri);
+
+// COURSE: Read Data in MongoDB
+
+// async function run() {
+//     try {
+//         await client.connect();
+//         // database and collection code goes here
+//         const db = client.db('sample_guides');
+//         const coll = db.collection('planets');
+//         // find code goes here
+//         const cursor = coll.find();
+//         // iterate code goes here
+//         await cursor.forEach(console.log);
+//     } finally {
+//         await client.close();
+//     }
+// }
+
 async function run() {
     try {
         await client.connect();
@@ -12,10 +30,11 @@ async function run() {
         const db = client.db('sample_guides');
         const coll = db.collection('planets');
         // find code goes here
-        const cursor = coll.find();
+        const cursor = coll.find({ hasRings: false, mainAtmosphere: 'Ar' });
         // iterate code goes here
         await cursor.forEach(console.log);
     } finally {
+        // Ensures thate the client will close when you finish/error
         await client.close();
     }
 }
