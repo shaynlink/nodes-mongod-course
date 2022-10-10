54693b6cdb8a852f30e242a4c5f505b8dfc71a8c
shaynlink <guemmoudrayane03@gmail.com>
2022-10-11 00:59:51 +0200

    course: Insert Data into MongoDB

--- src/index.js
+++ src/index.js
@@ -41,26 +41,51 @@ const client = new MongoClient(uri);
 //     }
 // }
 
+// COURSE: Read Date using Operators and Compound Queries
+
+// async function run() {
+//     try {
+//         await client.connect();
+//         const coll = client.db('sample_guides').collection('planets');
+
+//         // find code goes here
+//         // const cursor = coll.find({
+//         //     'surfaceTemperatureC.mean': { $lt: 15 },
+//         //     'surfaceTemperatureC.min': { $gt: -100 }
+//         // });
+
+//         // const cursor = coll.find({
+//         //     $and: [{ orderFromSun: { $gt: 2} }, { orderFromSun: { $lt: 5 } }]
+//         // });
+
+//         const cursor = coll.find({
+//             $or: [{ orderFromSun: { $gt: 7 } }, { orderFromSun: { $lt: 2 } }],
+//         });
+
+//         await cursor.forEach(console.log);
+//     } finally {
+//         // Ensures that the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+
 async function run() {
     try {
         await client.connect();
-        const coll = client.db('sample_guides').collection('planets');
-
-        // find code goes here
-        // const cursor = coll.find({
-        //     'surfaceTemperatureC.mean': { $lt: 15 },
-        //     'surfaceTemperatureC.min': { $gt: -100 }
-        // });
-
-        // const cursor = coll.find({
-        //     $and: [{ orderFromSun: { $gt: 2} }, { orderFromSun: { $lt: 5 } }]
-        // });
-
-        const cursor = coll.find({
-            $or: [{ orderFromSun: { $gt: 7 } }, { orderFromSun: { $lt: 2 } }],
-        });
-
-        await cursor.forEach(console.log);
+        // database and collection code goes here
+        const db = client.db('sample_guides');
+        const coll = db.collection('comets');
+        // insert code goes here
+        const docs = [
+            {name: "Halley's Comet", officialName: "1P/Halley", orbitalPeriod: 75, radius: 3.4175, mass: 2.2e14},
+            {name: "Wild2", officialName: "81P/Wild", orbitalPeriod: 6.41, radius: 1.5534, mass: 2.3e13},
+            {name: "Comet Hyakutake", officialName: "C/1996 B2", orbitalPeriod: 17000, radius: 0.77671, mass: 8.8e12}
+        ];
+  
+        const result = await coll.insertMany(docs);
+  
+        // display the results of your operation
+        console.log(result.insertedIds);
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
