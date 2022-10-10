702a24d823d810be0cb49768d11f9ce38f977447
shaynlink <guemmoudrayane03@gmail.com>
2022-10-11 00:54:59 +0200

    course: Read Data using Operators and Compound Queries

--- src/index.js
+++ src/index.js
@@ -23,18 +23,46 @@ const client = new MongoClient(uri);
 //     }
 // }
 
+// COURSE: Read Data from MongoDB With Queries
+
+// async function run() {
+//     try {
+//         await client.connect();
+//         // database and collection code goes here
+//         const db = client.db('sample_guides');
+//         const coll = db.collection('planets');
+//         // find code goes here
+//         const cursor = coll.find({ hasRings: false, mainAtmosphere: 'Ar' });
+//         // iterate code goes here
+//         await cursor.forEach(console.log);
+//     } finally {
+//         // Ensures thate the client will close when you finish/error
+//         await client.close();
+//     }
+// }
+
 async function run() {
     try {
         await client.connect();
-        // database and collection code goes here
-        const db = client.db('sample_guides');
-        const coll = db.collection('planets');
+        const coll = client.db('sample_guides').collection('planets');
+
         // find code goes here
-        const cursor = coll.find({ hasRings: false, mainAtmosphere: 'Ar' });
-        // iterate code goes here
+        // const cursor = coll.find({
+        //     'surfaceTemperatureC.mean': { $lt: 15 },
+        //     'surfaceTemperatureC.min': { $gt: -100 }
+        // });
+
+        // const cursor = coll.find({
+        //     $and: [{ orderFromSun: { $gt: 2} }, { orderFromSun: { $lt: 5 } }]
+        // });
+
+        const cursor = coll.find({
+            $or: [{ orderFromSun: { $gt: 7 } }, { orderFromSun: { $lt: 2 } }],
+        });
+
         await cursor.forEach(console.log);
     } finally {
-        // Ensures thate the client will close when you finish/error
+        // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
